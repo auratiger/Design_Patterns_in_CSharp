6ae4bcece8cb04e4c5f424c02873217ae87f3d77
auratiger <auratiger00@gmail.com>
2020-11-22 18:44:10 +0200

    CompositePattern update

--- Design Patterns/Structural Patterns/CompositePattern.cs
+++ Design Patterns/Structural Patterns/CompositePattern.cs
@@ -4,6 +4,38 @@ using System.Text;
 
 namespace Design_Patterns.Structural_Patterns
 {
+    /*
+     * The Composite is a structural design pattern that lets you compose
+     * objects into tree structures and they work with these structures
+     * as if they were individual objects
+     *
+     * Using the Composite pattern makes sense only when the core model
+     * of your app can be represented as a tree.
+     *
+     * For example, imagine that you have two types of objects: Products and Boxes
+     * A Box can contain several Products as well as a number of even smaller Boxes,
+     * and so on.
+     *
+     * Say you decide to create an ordering system that uses these classes.
+     * Orders could contain simple products without any wrapping,
+     * as well as boxes stuffed with products... and other boxes.
+     * How would you determine the total price of such an order?
+     *
+     * You could try the direct approach: unwrap all the boxes,
+     * go over all the products and then calculate the total.
+     * That would be doable in the real world; but in a program,
+     * it's not as simple as running a loop. You have to know the classes
+     * of Products and Boxes you are going through, the nesting level of
+     * the nesting level of the boxes and other nasty details beforehand.
+     * All of this makes the direct approach either too awkward or ever impossible.
+     *
+     * The Composite pattern suggest that you work with Products And Boxes through
+     * a common interface which declares a method for calculating the total price.
+     * The greates benefit of this approach is that you don't need to care about
+     * the concrete classes of objects that compose the tree.
+     * You can treat them all the same via the common interface.
+     * When you call a method, the objects themselves pass the request down the tree.
+     */
     public class CompositePattern
     {
         public static void Test()
